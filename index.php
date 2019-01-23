4dac44f87281d4d72d67aa0b70865d7af888dea4
nisol91 <nicolasolzi91@gmail.com>
2019-01-23 13:18:36 +0100

    1

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link href="https://fonts.googleapis.com/css?family=Lato:400,900" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+    <title></title>
+  </head>
+  <body>
+    <div class="container">
+      <div class="nav">
+        <div class="up_nav">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1200px-Google_2015_logo.svg.png" alt="">
+          <h2>Privacy & Terms</h2>
+        </div>
+        <div class="down_nav">
+          <a href="#"><h3></h3></a>  
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
